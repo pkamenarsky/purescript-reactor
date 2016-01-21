82d7f303483718b8c92bb7f2f5e5791a27489621
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-01-21 22:14:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bp = require('body-parser');
+
+app.use(bp.raw());
+
+var state = {};
+
+app.post('/set', function(req, res){
+    console.dir(req.body);
+    state = req.body;
+    res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+    res.end('{\"result\": \"ok\"}');
+});
+
+app.get('/get', function(req, res){
+    res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+    res.end(state);
+});
+
+port = 8005;
+app.listen(port);
+console.log('Listening at http://localhost:' + port)
